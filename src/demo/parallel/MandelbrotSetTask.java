9409298c678a0327b0cfde543fb693db0412d094
Alexrshut <aleksrshut@yandex.by>
2024-09-19 09:43:03 +0300

    Изменил цветовую гамму на жёлто-оранжевую

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,14 +351,15 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+                Color.rgb(40, 20, 0),
+                Color.ORANGE,
+                Color.YELLOW,
+                Color.ORANGE,
+                Color.rgb(100, 50, 0),
+                Color.ORANGE,
+                Color.rgb(50, 25, 0)
         };
+
         
         /**
          * Color stops for colors table: relative position in the table
